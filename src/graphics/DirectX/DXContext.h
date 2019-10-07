776248cb6093bca26f42adf3762c891615b14e13
J4m3s00 <james.lol2000@gmail.com>
2019-10-07 23:11:54 +0200

    Update context

--- src/graphics/DirectX/DXContext.h
+++ src/graphics/DirectX/DXContext.h
@@ -11,6 +11,11 @@ public:
 	void Clear(const D3DXCOLOR& color = D3DXCOLOR(0.5f, 0.7f, 0.74f, 1.0f));
 	void Present();
 
+	IDXGISwapChain* GetSwapChain() const { return fpSwapChain; }
+	ID3D11Device* GetDevice() const { return fpDevice; }
+	ID3D11DeviceContext* GetDeviceContext() const { return fpDeviceContext; }
+	//ID3D11RenderTargetView* GetBackBuffer() const { return fpBackBuffer; }
+
 	static void Init(const Window& wnd);
 private:
 	IDXGISwapChain* fpSwapChain = nullptr;
