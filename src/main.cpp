20d426c88d8361cfccf427836b03e032388968f7
J4m3s00 <james.lol2000@gmail.com>
2019-10-08 20:38:44 +0200

    Drawing triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "prefix.h"
 #include <iostream>
 
+struct Vertex
+{
+	float x;
+	float y; 
+	float z;
+	D3DXCOLOR Color;
+};
+
 int CALLBACK WinMain(_In_ HINSTANCE hInstace, _In_opt_ HINSTANCE hPrevInstace, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	Window* window = new Window(1270, 720, "Hello World");
@@ -10,6 +18,7 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstace, _In_opt_ HINSTANCE hPrevInstace, _
 	D3DX11CompileFromFile("src/shader/Basic.hlsl", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, 0, 0);
 	D3DX11CompileFromFile("src/shader/Basic.hlsl", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, 0, 0);
 
+	//Global
 	ID3D11VertexShader* pVS; 
 	ID3D11PixelShader* pPS;
 
@@ -20,6 +29,46 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstace, _In_opt_ HINSTANCE hPrevInstace, _
 	auto devcon = DXContext::sInstance.GetDeviceContext();
 	devcon->VSSetShader(pVS, 0, 0);
 	devcon->PSSetShader(pPS, 0, 0);
+
+
+
+	//Input Layout
+	//Global
+	ID3D11InputLayout* pLayout;
+
+	D3D11_INPUT_ELEMENT_DESC inputDescription[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 3 * sizeof(float), D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+	device->CreateInputLayout(inputDescription, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &pLayout);
+	devcon->IASetInputLayout(pLayout);
+
+
+	//Init VBO
+	Vertex vertices[] =
+	{
+		{-0.5f, -0.5f, 0.0f, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)},
+		{ 0.0f,  0.5f, 0.0f, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)},
+		{ 0.5f, -0.5f, 0.0f, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)},
+	};
+
+	//Global
+	ID3D11Buffer* pVBuffer;
+
+	D3D11_BUFFER_DESC bd = {};
+	bd.Usage = D3D11_USAGE_DYNAMIC;
+	bd.ByteWidth = sizeof(vertices);
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	device->CreateBuffer(&bd, NULL, &pVBuffer);
+
+	D3D11_MAPPED_SUBRESOURCE ms;
+	devcon->Map(pVBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+	memcpy(ms.pData, vertices, sizeof(vertices));
+	devcon->Unmap(pVBuffer, NULL);
+
 
 	MSG msg;
 	while (TRUE)
@@ -34,6 +83,14 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstace, _In_opt_ HINSTANCE hPrevInstace, _
 		else
 		{
 			DXContext::sInstance.Clear();
+
+			UINT stride = sizeof(Vertex);
+			UINT offset = 0;
+			devcon->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
+
+			devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+			devcon->Draw(3, 0);
 
 			DXContext::sInstance.Present();
 		}
