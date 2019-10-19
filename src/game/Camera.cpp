d36b85b7373e54cb0f471459dd20157ab0f549e3
J4m3s00 <james.lol2000@gmail.com>
2019-10-19 18:07:20 +0200

    Camera class

---
+++ src/game/Camera.cpp
@@ -0,0 +1,6 @@
+#include <prefix.h>
+
+FpsCamera::FpsCamera()
+{
+
+}
--- src/game/Camera.h
+++ src/game/Camera.h
@@ -1,2 +1,24 @@
 #pragma once
 
+//Camera interface
+class Camera
+{
+public:
+	virtual const DirectX::XMMATRIX& GetViewMatrix() const = 0;
+
+	virtual void Update(float delta) = 0;
+};
+
+class FpsCamera : public Camera
+{
+public:
+	FpsCamera();
+
+	virtual const DirectX::XMMATRIX& GetViewMatrix() const override;
+	virtual void Update(float delta) override;
+private:
+	DirectX::XMMATRIX fViewMatrix;
+
+	DirectX::XMVECTOR fPosition;
+	DirectX::XMVECTOR fRotation;
+};
